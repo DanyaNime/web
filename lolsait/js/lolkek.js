3d81fbc5ce7e7a24eadd73ba3189af591631110a
DanyaNime <36900978+DanyaNime@users.noreply.github.com>
2018-03-14 18:53:39 +0300

    Add files via upload

---
+++ lolsait/js/lolkek.js
@@ -0,0 +1,112 @@
+/*var debil = prompt("напишите конченый", " конченый");
+alert("Поздравляю, вы" + debil +" дебил!");
+var viAdm = confirm("Вы - Администратор?");
+alert(viAdm); 
+var imya = prompt("Могу ли я узнать ваше имя?","");
+alert("Добро пожаловать," + imya +"!");
+
+var someThing = prompt("Какого числа октрября у Дани день рождения?", "");
+
+if (someThing < 31){
+	alert("Рановато, фраерок!");
+} else if (someThing > 31) {
+	alert("Далековато, псина!")
+} else {
+	alert("Красава, не сомневался в тебе браток!")
+}
+
+var access;
+var age = prompt("Скок годков то тебе?", "");
+if (age => 14) {
+	access = true;
+} else {
+	access = false;
+}
+alert(access); 
+
+var niceTry = prompt("Годов скок те?", 18);
+var message = (niceTry < 3) ? "Здарова малой." :
+(niceTry < 18) ? "Здарова прыщавый." :
+(niceTry < 60) ? "Здравствуйте" :
+"ТЫ кого нае*ать решил, петух?!";
+alert(message); 
+
+var skip = prompt("Официальное название javascript?", "");
+if (skip == "EcmaScript") {
+	alert("ПРАВИЛЬНО!");
+} else {
+	alert("Не знаешь? EcmaScript!");
+}
+
+var chush = prompt("Введите число", 0);
+
+if (chush > 0) { 
+	alert( 1 );
+} else if (chush < 0) {
+	alert( -1 );
+} else  {
+	alert( 0 );
+} 
+
+var userName = prompt("Кто пришел?", "");
+
+if (userName == "Админ") {
+
+	var pass = prompt("Пароль?", "");
+	if (pass == "Черный Властелин") {
+		alert("Добро Пожаловать!");
+	}
+	else if (pass == null) {
+		alert("Выход отменен");
+	}
+	else {
+		alert("Пароль неверен");
+	}
+} else if (userName == null) {
+	alert("Выход отменен");
+} else {
+	alert("Я вас не знаю");
+} 
+
+for (var i = 2; i <= 10; i++) {
+	if (i % 5 == 0) {
+		alert(i);
+	}
+} */
+
+/*var a = +prompt('a?', '');
+
+if (a == 0) {
+  alert( 0 );
+}
+if (a == 1) {                    //ПРИМЕР С IF
+  alert( 1 );
+}
+
+if (a == 2 || a == 3) {
+  alert( '2,3' );
+}
+
+var a = +prompt("a?", "");
+
+switch (a) {
+	case 0:
+	alert( 0 );
+	break;                    // ТОЖЕ САМОЕ ТОЛЬКО SWITCH
+
+	case 1:
+	alert( 1 );
+	break;
+
+	case 2:
+	case 3:
+	alert( "2,3" );
+	break;
+} 
+
+for (var i = 2; i <= 10; i++) {
+	if (i % 5 == 0) {
+		alert(i);
+	}
+}
+*/
